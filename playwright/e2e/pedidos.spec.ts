bea79f4ca51aed67b801f211e529e6bb795782f5
Leonardo Euzebio <diverleosp@gmail.com>
2026-06-12 11:06:14 -0300

    implementado snapshot no aprovado e reprovado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -23,13 +23,26 @@ test.describe('Consulta de Pedido', ()=>{
   test('deve consultar um pedido aprovado', async ({ page }) => {
 
     // teste data
-    const order = 'VLO-290N33' // numero do pedido
+    //const order = 'VLO-290N33' // numero do pedido
     //Arrange
+    //criando um objeto
+    const order = {
+      number: 'VLO-290N33',
+      status: 'APROVADO',
+      color: 'Midnight Black',
+      wheels: 'aero wheels',
+      custumer: {
+        name: 'leo euzebio',
+        email: 'diver@velo.dev.com',
+
+    },
+    payment: 'À Vista',
+    }
    
     // Act
     //mudando o nome do input para order-id
     //await page.locator('input[name="order-id"]').fill('VLO-290N33');
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
     //await page.getByTestId('search-order-button').click();
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
   
@@ -44,32 +57,32 @@ test.describe('Consulta de Pedido', ()=>{
    */
 
     //teste de snapshot
-    await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
       - img
       - paragraph: Pedido
-      - paragraph: ${order}
+      - paragraph: ${order.number}
       - img
-      - text: APROVADO
+      - text: ${order.status}
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
       - paragraph: Cor
-      - paragraph: Midnight Black
+      - paragraph: ${order.color}
       - paragraph: Interior
       - paragraph: cream
       - paragraph: Rodas
-      - paragraph: aero Wheels
+      - paragraph: ${order.wheels}
       - heading "Dados do Cliente" [level=4]
       - paragraph: Nome
-      - paragraph: leo euzebio
+      - paragraph: ${order.custumer.name}
       - paragraph: Email
-      - paragraph: diver@velo.dev.com
+      - paragraph: ${order.custumer.email}
       - paragraph: Loja de Retirada
       - paragraph
       - paragraph: Data do Pedido
       - paragraph: /\\d+\\/\\d+\\/\\d+/
       - heading "Pagamento" [level=4]
-      - paragraph: À Vista
+      - paragraph: ${order.payment}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
       `);
     
@@ -110,7 +123,72 @@ test.describe('Consulta de Pedido', ()=>{
       - heading "Pedido não encontrado" [level=3]
       - paragraph: Verifique o número do pedido e tente novamente
       `);
+  });
+  test('deve consultar um pedido reprovado', async ({ page }) => {
+
+    // teste data
+    //const order = 'VLO-9GKXPY' // numero do pedido
+    //Arrange
+    const order = {
+      number: 'VLO-9GKXPY',
+      status: 'REPROVADO',
+      color: 'Lunar White',
+      wheels: 'Sport Wheels',
+      custumer: {
+        name: 'Bruce Euzebio',
+        email: 'bruce@iron.com.br',
+
+    },
+    payment: 'À Vista',
+    }
+
+    // Act
+    //mudando o nome do input para order-id
+    //await page.locator('input[name="order-id"]').fill('VLO-290N33');
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
+    //await page.getByTestId('search-order-button').click();
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
   
+    //Assert
+    //await page.waitForTimeout(10000);//thead sleep ou cypress wait não é correto usar esse modo de esperar
+ 
+
+    /* //estes são utilizando os IDs
+    await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000});
+    await expect(page.getByTestId('order-result-id')).toContainText(order);
+    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+   */
+
+    //teste de snapshot
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.custumer.name}
+      - paragraph: Email
+      - paragraph: ${order.custumer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
   });  
 })
 
